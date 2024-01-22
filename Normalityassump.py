def0d1da0d74df3d7e0f7d2d2ab3d5da6f4cce0c
AliALTINT <altintasal@mef.edu.tr>
2024-01-22 23:46:28 +0300

    Succes

--- Normalityassump.py
+++ Normalityassump.py
@@ -1,4 +1,22 @@
 from scipy.stats import shapiro
+import pandas as pd
+import numpy as np
+# Set a random seed for reproducibility
+np.random.seed(42)
+
+# Generate sample data for Group A (NFC withdrawal amount) with an exponential distribution
+data_group_a = {
+    'WithDrawalAmount_USD': np.random.exponential(scale=50.000, size=34)
+}
+df_group_a = pd.DataFrame(data_group_a)
+df_group_a.WithDrawalAmount_USD.mean()
+
+# Generate sample data for Group A (NFC withdrawal amount) with an exponential distribution
+data_group_b = {
+    'WithDrawalAmount_USD': np.random.exponential(scale=60.000, size=34)
+}
+df_group_b = pd.DataFrame(data_group_b)
+df_group_b.WithDrawalAmount_USD.mean()
 
 # Shapiro-Wilk test for normality
 stat_group_a, p_value_group_a = shapiro(df_group_a['WithDrawalAmount_USD'])
--- man-whitney-you.py
+++ man-whitney-you.py
@@ -1,4 +1,18 @@
 from scipy.stats import mannwhitneyu
+import numpy as np
+import pandas as pd
+
+# Generate sample data for Group A (NFC withdrawal amount) with an exponential distribution
+data_group_a = {
+    'WithDrawalAmount_USD': np.random.exponential(scale=50.000, size=34)
+}
+df_group_a = pd.DataFrame(data_group_a)
+
+# Generate sample data for Group A (NFC withdrawal amount) with an exponential distribution
+data_group_b = {
+    'WithDrawalAmount_USD': np.random.exponential(scale=60.000, size=34)
+}
+df_group_b = pd.DataFrame(data_group_b)
 
 # Mann-Whitney U test
 u_statistic, p_value = mannwhitneyu(df_group_a['WithDrawalAmount_USD'], df_group_b['WithDrawalAmount_USD'])
