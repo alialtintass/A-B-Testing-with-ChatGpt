8cdd667f07a5dd808c28141e7bbcb7a55cc33b5d
Ali Altıntaş <alialtintas@yahoo.com>
2024-02-10 13:15:47 +0300

    Update Normalityassump.py

--- Normalityassump.py
+++ Normalityassump.py
@@ -4,13 +4,13 @@ import numpy as np
 # Set a random seed for reproducibility
 np.random.seed(42)
 
-# Generate sample data for Group A (NFC withdrawal amount) with an exponential distribution
+# Generate sample data for Group A (QR Code withdrawal amount) with an Gamma distribution
 data_group_a = {
     'WithDrawalAmount_USD': np.random.gamma(2, 65.000, 34)
 }
 df_group_a = pd.DataFrame(data_group_a)
 
-# Generate sample data for Group A (NFC withdrawal amount) with an exponential distribution
+# Generate sample data for Group A (Debit Card withdrawal amount) with an Gamma distribution
 data_group_b = {
     'WithDrawalAmount_USD': np.random.gamma(2, 50.000, 34)
 }
@@ -25,3 +25,8 @@ stat_group_b, p_value_group_b = shapiro(df_group_b['WithDrawalAmount_USD'])
 print("Shapiro-Wilk Test for Normality:")
 print(f"Group A - W-statistic: {stat_group_a}, p-value: {p_value_group_a}")
 print(f"Group B - W-statistic: {stat_group_b}, p-value: {p_value_group_b}")
+
+
+
+
+
